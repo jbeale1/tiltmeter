5cb76321ba40e47a4714df91e1bc7073ad3184b0
John Beale <7452231+jbeale1@users.noreply.github.com>
2024-12-12 22:00:51 -0800

    add flush to disk

--- tilt1.py
+++ tilt1.py
@@ -4,22 +4,33 @@
 import serial, os
 from datetime import datetime
 
-logDir = r'/home/john/Documents/tiltmeter'
+#logDir = r'/home/john/Documents/tiltmeter'
+logDir = r'C:\Users\beale\Documents\Tiltmeter'
+
+flushCount = 20   # how many lines to buffer before forcing data to disk file
+
 now = datetime.now()
 tsLog = now.strftime("%Y%m%d_%H%M%S_tilt3.csv")
 logfile = os.path.join(logDir, tsLog)
 
+sPort = 'COM7'  # serial port
+# sPort='/dev/ttyACM1'
 
 # Configure the serial port
 ser = serial.Serial(
-    port='/dev/ttyACM1', 
+    port=sPort, 
     baudrate=115200,
     timeout=1  # Timeout for reading
 )
 
+fc = 0
 with open(logfile, 'w') as f:
     while True:
         line = ser.readline().decode('utf-8').strip()
-        if line:
+        if line:            
             print(line)  # Optional: Print to console
             f.write(line + '\n')
+            fc += 1
+            if (fc > flushCount):
+                f.flush()
+                fc=0
